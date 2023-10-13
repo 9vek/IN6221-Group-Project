6e010eed8bfe37c2250373cd706a1285a2b018a3
Kevyn <39913345+KevynTang@users.noreply.github.com>
2023-10-14 00:51:13 +0800

    setup project framework

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import * as d3 from "d3"
+
+const data = [10, 20, 30, 40, 50]
+const width = 500
+const height = 300
+const margin = { top: 20, right: 20, bottom: 30, left: 40 }
+
+const drawTest = () => {
+  // Set up scales
+  const xScale = d3.scaleBand()
+    .domain(data.map((d, i) => i.toString()))
+    .range([margin.left, width - margin.right])
+    .padding(0.1)
+
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data) || 0])
+    .range([height - margin.bottom, margin.top])
+
+  // Create SVG canvas
+  const svg = d3.select(".svg-canvas")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+  // Draw bars
+  svg.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => xScale(i.toString()))
+    .attr("y", d => yScale(d))
+    .attr("width", xScale.bandwidth())
+    .attr("height", d => height - margin.bottom - yScale(d))
+    .attr("fill", "steelblue")
+}
+
+onMounted(() => {
+  drawTest()
+  drawTest()
+})
+</script>
+
+<template>
+  <div class='svg-canvas'></div>
+</template>
+
+<style scoped>
+</style>
