e390922f156378d7bc58d1c976bf741d4abbf2af
Tonymiugrey <tonymiugrey@qq.com>
2023-11-09 22:05:30 +0800

    map switch data but buggy

--- src/components/charts/MapWithBubble.vue
+++ src/components/charts/MapWithBubble.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-import { defineProps, onMounted } from 'vue'
+import { ref, defineProps, onMounted } from 'vue'
 import * as d3 from "d3"
 
 const props = defineProps({
-  file: String
+  file1: String,
+  file2: String,
 })
 
-const drawTest = (file: String) => {
+const updateData = ref<(input: string) => void>(() => {})
+
+const drawTest = (file1: String, file2: String, updateDataRef: any) => {
   // set width and height of svg
-  const width = 1200
-  const height = 600
+  const width = 600
+  const height = 400
 
   // The svg
   const svg = d3.select("#map")
@@ -19,17 +22,19 @@ const drawTest = (file: String) => {
 
   // Map and projection
   const projection = d3.geoMercator()
-    .center([103.84, 1.32])                // GPS of location to zoom on
-    .scale(width * 90)                       // This is like the zoom
+    .center([103.85, 1.32])                // GPS of location to zoom on
+    .scale(width * 116)                       // This is like the zoom
     .translate([width / 2, height / 2])
 
   // Load external data and boot
   Promise.all([
     d3.json("./src/data/geo/Singapore.geojson"),
-    d3.json(file as string)
+    d3.json(file1 as string),
+    d3.json(file2 as string)
   ]).then(function (data) {
     let mapData = data[0]
-    let infoData = data[1]
+    let infoData1 = data[1]
+    let infoData2 = data[2]
 
     // Draw the map
     svg.append("g")
@@ -42,6 +47,10 @@ const drawTest = (file: String) => {
       )
       .style("stroke", "black")
       .style("opacity", .3)
+
+      const color = d3.scaleOrdinal()
+      .domain(["A", "B", "C" ])
+      .range([ "#402D54", "#D18975", "#8FD175"])
 
     // create a tooltip
     const Tooltip = d3.select("#map")
@@ -77,33 +86,47 @@ const drawTest = (file: String) => {
       Tooltip.style("opacity", 0)
     }
 
-    // Add circles:
-    svg
-      .selectAll("myCircles")
-      .data(infoData.features)
-      .join("circle")
-      .attr("cx", d => projection(d.geometry.coordinates)[0])
-      .attr("cy", d => projection(d.geometry.coordinates)[1])
-      .attr("r", 8)
-      .attr("class", "circle")
-      .style("fill", "69b3a2")
-      .attr("stroke", "#69b3a2")
-      .attr("stroke-width", 1)
-      .attr("fill-opacity", .3)
-      .on("mouseover", mouseover)
-      .on("mousemove", mousemove)
-      .on("mouseleave", mouseleave)
+    updateDataRef.value = (input: string) => {
+      var data = infoData1
+
+        if (input == "2") {
+          data = infoData2
+        }
+
+        if (input == "1") {
+          data = infoData1
+        }
+          // Add circles
+      svg
+        .selectAll("myCircles")
+        .data(data.features)
+        .join("circle")
+        .attr("cx", d => projection(d.geometry.coordinates)[0])
+        .attr("cy", d => projection(d.geometry.coordinates)[1])
+        .attr("r", 8)
+        .attr("class", "circle")
+        .style("fill", "69b3a2")
+        .attr("stroke", "#69b3a2")
+        .attr("stroke-width", 1)
+        .attr("fill-opacity", .3)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+    }
   })
 }
 
 onMounted(() => {
-  drawTest(props.file as string)
+  drawTest(props.file1 as string, props.file2 as string, updateData)
 })
-
 </script>
 
 <template>
-  <div id="map" class="w-fit h-fit"></div>
+  <div>
+    <div @click="updateData('1')" class="btn"> Data1 </div>
+    <div @click="updateData('2')" class="btn"> Data2 </div>
+    <div id="map" class="w-full h-full"></div>
+  </div>
 </template>
 
 <style scoped>
--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -1,13 +1,15 @@
 <script setup lang='ts'>
 import MapBubble from '../components/charts/MapWithBubble.vue'
 import TextWithTitle from '../components/charts/TextWithTitle.vue';
-const ewasteData = './src/data/geo/filtered_WasteTreatmentGEOJSON.geojson'
+const ewasteData = './src/data/geo/filtered_EwasteRecycling.geojson'
+const wasteTreatmentData = './src/data/geo/filtered_WasteTreatmentGEOJSON.geojson'
+
 </script>
 
 <template>
   <div class="bg-primary h-screen grid grid-cols-3 place-items-center">
     <div class="col-span-2 grid grid-cols-1 place-items-center bg-yellow-100 rounded-lg p-16">
-      <MapBubble :file="ewasteData" />
+      <MapBubble :file1="ewasteData" :file2="wasteTreatmentData" />
     </div>
     <TextWithTitle :title="'test'" :text="'test test test'" />
   </div>
