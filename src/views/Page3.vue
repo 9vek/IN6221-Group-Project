ac638a246bd1c7d299f863fbdef161a64ecb5ebc
Tonymiugrey <tonymiugrey@qq.com>
2023-11-10 12:40:03 +0800

    bg for donut

--- src/views/Page3.vue
+++ src/views/Page3.vue
@@ -7,6 +7,16 @@ const pieChartTest = ref('./src/data/csv/RecyclingRateByWasteType/')
 </script>
 
 <template>
+  <div class="bg-accent h-screen grid grid-cols-3 place-items-center">
+    <div class="col-span-2 grid grid-cols-1 place-items-center bg-purple-50 rounded-[32px] p-16 mx-12">
+      <div class="flex justify-center items-center">
+        <!-- <PieChart :file="pieChartTest"/> -->
+        <DonutChart :file="pieChartTest"/>
+      </div>
+    </div>
+    <TextWithTitle :title="'test'" :text="'test test test'" />
+  </div>
+
     <div class="h-screen flex justify-center items-center bg-accent">
       <div class="flex justify-center items-center">
         <!-- <PieChart :file="pieChartTest"/> -->
