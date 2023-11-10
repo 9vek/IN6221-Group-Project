5662fd76674a4c41178ee7356870e9f426d2c9fc
hanqing guan <z5213081@unsw.edu.au>
2023-11-10 15:50:44 +0800

    fix item5 size

--- src/views/Page5.vue
+++ src/views/Page5.vue
@@ -11,15 +11,30 @@ const solidEnergy = ref('./src/data/csv/SolidWasteManagementEnergyProducedFromIn
 
 <template>
   <div class="bg-accent h-screen grid grid-cols-3">
+    <div class="col-span-2 bg-purple-50 rounded-r-[32px] w-full h-full">
+      <div class="chart-container">
+        <LineChart :file="solidEnergy" :title="solidEnergyTitle" :y-text="solidEnergyY"/>
+      </div>
+    </div>
     <div class="grid-flow-col auto-cols-max place-items-center">
       <TextWithTitle :title="'Recyling Rate by Waste Type'"
         :text="'There are three types of waste with highest recycling rate during 2000 to 2015: Construction Debris, Used Slag, and Ferrous Metal.'" />
-        <img src='../../guide_img/folder_termi.png' alt="Descriptive text for the image" class="h-auto rounded-[16px] m-4" />
-    </div>
-    <div class="col-span-2 bg-purple-50 rounded-l-[32px] w-full h-full">
-      <LineChart :file="solidEnergy" :title="solidEnergyTitle" :y-text="solidEnergyY"/>
-    </div>
+        <img src='../../guide_img/folder_termi.png' alt="Descriptive text for the image" class="h-auto rounded-[16px]" />
+    </div> 
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.chart-container {
+  margin-top: 20px;
+  width: 90%; 
+  height: 90%;
+  padding: 2rem; 
+  box-sizing: border-box; 
+  overflow: hidden; 
+  display: flex; 
+  align-items: center;
+  justify-content: center; 
+}
+
+</style>
