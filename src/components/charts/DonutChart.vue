1f67c1fbbee97cbe72b30e22895ad1882c5aa0f9
Tonymiugrey <tonymiugrey@qq.com>
2023-11-13 02:12:41 +0800

    DONUT ANIMATION!!!!!!!!!!!

--- src/components/charts/DonutChart.vue
+++ src/components/charts/DonutChart.vue
@@ -2,25 +2,27 @@
 import { defineProps, onMounted, ref } from 'vue'
 import * as d3 from "d3"
 
-const selectedYear = ref(2000);
+const selectedYear = ref(0);
+
+const updateData = ref<(input: number) => void>(() => { });
 
 const nextYear = () => {
-  if (selectedYear.value < 2015) {
+  if (selectedYear.value < 15) {
     selectedYear.value += 1
-    drawTest(props.file);
+    updateData.value(selectedYear.value);
   }
 };
 
 const previousYear = () => {
-  if (selectedYear.value > 2000) {
+  if (selectedYear.value > 0) {
     selectedYear.value -= 1
-    drawTest(props.file);
+    updateData.value(selectedYear.value);
   }
 };
 
 const handleYearChange = (event) => {
   selectedYear.value = Number(event.target.value);
-  drawTest(props.file);
+  updateData.value(selectedYear.value);
 };
 
 const props = defineProps({
@@ -40,11 +42,7 @@ const getPercentageForLabel = function (d) {
   return Math.round(percentage * 1000) / 10
 }
 
-const drawTest = (filePath: string) => {
-  const file = filePath + selectedYear.value + '.csv';
-  console.log(file)
-  d3.select("#donutChart").selectAll("*").remove();
-
+const drawTest = (filePath: string, updateDataRef: any, selectedYearRef: any) => {
   // set the dimensions and margins of the graph
   const container = d3.select("#donutChart");
   const width = container.node().getBoundingClientRect().width;
@@ -81,15 +79,37 @@ const drawTest = (filePath: string) => {
     .innerRadius(radius * 0.9)
     .outerRadius(radius * 0.9)
 
-  d3.csv(file).then(function (data) {
-    const pieData = pie(data);
+  Promise.all([
+    d3.csv(filePath + '/2000.csv'),
+    d3.csv(filePath + '/2001.csv'),
+    d3.csv(filePath + '/2002.csv'),
+    d3.csv(filePath + '/2003.csv'),
+    d3.csv(filePath + '/2004.csv'),
+    d3.csv(filePath + '/2005.csv'),
+    d3.csv(filePath + '/2006.csv'),
+    d3.csv(filePath + '/2007.csv'),
+    d3.csv(filePath + '/2008.csv'),
+    d3.csv(filePath + '/2009.csv'),
+    d3.csv(filePath + '/2010.csv'),
+    d3.csv(filePath + '/2011.csv'),
+    d3.csv(filePath + '/2012.csv'),
+    d3.csv(filePath + '/2013.csv'),
+    d3.csv(filePath + '/2014.csv'),
+    d3.csv(filePath + '/2015.csv')
+  ]).then(function (data) {
 
-    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    const slices = svg
-      .selectAll('allSlices')
-      .data(pieData);
+    updateDataRef.value = (input: number) => {
+      console.log(input)
+      d3.select("#donutChart").selectAll("text").remove();
+      d3.select("#donutChart").selectAll("polyline").remove();
+      
+      const pieData = pie(data[input])
 
-    slices
+      const slices = svg
+        .selectAll('path')
+        .data(pieData);
+
+      slices
       .enter()
       .append('path')
       .merge(slices)
@@ -101,70 +121,70 @@ const drawTest = (filePath: string) => {
       .attrTween("d", function (d) {
         // Use d3.interpolate to compute the smooth transition between the old and new data
         const previous = this._current || { startAngle: 0, endAngle: 0 };
-        console.log(previous)
         const interpolate = d3.interpolate(previous, d);
-        console.log(interpolate)
         this._current = interpolate(1); // Save the current data for the next update
-        console.log(this._current)
         return function (t) {
           return arc(interpolate(t));
         };
       });
 
-    slices.exit().remove(); // Remove any unnecessary slices
+      slices.exit().remove(); // Remove any unnecessary slices
 
 
 
-    // Add the polylines between chart and labels:
-    svg
-      .selectAll('allPolylines')
-      .data(pie(data))
-      .join('polyline')
-      .filter(d => isSliceLargeEnough(d))
-      .attr("stroke", "black")
-      .style("fill", "none")
-      .attr("stroke-width", 1)
-      .attr('points', function (d) {
-        const posA = arc.centroid(d) // line insertion in the slice
-        const posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
-        const posC = outerArc.centroid(d); // Label position = almost the same as posB
-        const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
-        posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
-        return [posA, posB, posC]
-      })
-      .style("opacity", 0) // Set initial opacity to 0
-      .transition()
-      .duration(1000)
-      .style("opacity", 1); // Transition opacity to 1
+      // Add the polylines between chart and labels:
+      svg
+        .selectAll('allPolylines')
+        .data(pieData)
+        .join('polyline')
+        .filter(d => isSliceLargeEnough(d))
+        .attr("stroke", "black")
+        .style("fill", "none")
+        .attr("stroke-width", 1)
+        .attr('points', function (d) {
+          const posA = arc.centroid(d) // line insertion in the slice
+          const posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+          const posC = outerArc.centroid(d); // Label position = almost the same as posB
+          const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+          posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+          return [posA, posB, posC]
+        })
+        .style("opacity", 0) // Set initial opacity to 0
+        .transition()
+        .duration(1000)
+        .style("opacity", 1); // Transition opacity to 1
 
 
-    // Add the polylines between chart and labels:
-    svg
-      .selectAll('allLabels')
-      .data(pie(data))
-      .join('text')
-      .text(d => isSliceLargeEnough(d) ? d.data.waste_type + " " + String(getPercentageForLabel(d)) + "%" : '')
-      .attr('transform', function (d) {
-        const pos = outerArc.centroid(d);
-        const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-        pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
-        pos[1] = pos[1] + 6;
-        return `translate(${pos})`;
-      })
-      .style('text-anchor', function (d) {
-        const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-        return (midangle < Math.PI ? 'start' : 'end')
-      })
-      .style("font-size", "14px")  // 设置字体大小为12像素
-      .style("opacity", 0) // Set initial opacity to 0
-      .transition()
-      .duration(1000)
-      .style("opacity", 1); // Transition opacity to 1
+      // Add the polylines between chart and labels:
+      svg
+        .selectAll('allLabels')
+        .data(pieData)
+        .join('text')
+        .text(d => isSliceLargeEnough(d) ? d.data.waste_type + " " + String(getPercentageForLabel(d)) + "%" : '')
+        .attr('transform', function (d) {
+          const pos = outerArc.centroid(d);
+          const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+          pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+          pos[1] = pos[1] + 6;
+          return `translate(${pos})`;
+        })
+        .style('text-anchor', function (d) {
+          const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+          return (midangle < Math.PI ? 'start' : 'end')
+        })
+        .style("font-size", "14px")  // 设置字体大小为12像素
+        .style("opacity", 0) // Set initial opacity to 0
+        .transition()
+        .duration(1000)
+        .style("opacity", 1); // Transition opacity to 1
+      }
+
+    updateDataRef.value(selectedYearRef.value)
   })
 }
 
 onMounted(() => {
-  drawTest(props.file)
+  drawTest(props.file, updateData, selectedYear)
 })
 
 </script>
@@ -176,7 +196,7 @@ onMounted(() => {
         <button @click="previousYear" class="join-item btn btn-outline">Previous</button>
         <button @click="nextYear" class="join-item btn btn-outline">Next</button>
       </div>
-      <input :value="selectedYear" type="range" min="2000" max="2015" class="range" step="1" @input="handleYearChange" />
+      <input :value="selectedYear" type="range" min="0" max="15" class="range" step="1" @input="handleYearChange" />
       <div class="w-full flex justify-between text-xl px-2">
         <span>2000</span>
         <span>2003</span>
